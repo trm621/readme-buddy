f4bf7e4dafb1fb6b42608d95c7097d56e45866d3
trm621 <t.maher621@gmail.com>
2021-11-27 15:51:39 -0500

    added table of contents section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,7 +16,12 @@ function generateMarkdown(data) {
   ## Description
   ### ${data.description}
   ## Table of Contents
-  ### ${data.tableofcontents}
+  1. <a href="#installation">Installation</a>
+  2. <a href="#usage">Usage</a>
+  3. <a href="#license">License</a>
+  4. <a href="#contributing">Contributing</a>
+  5. <a href="#tests">Tests</a>
+  6. <a href="#questions">Questions</a>
   ## Installation
   ### ${data.installation-instructions}
   ## Usage
