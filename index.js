f9f673fae378f06dabe5213d301c6aaef42be26a
trm621 <t.maher621@gmail.com>
2021-11-27 00:36:21 -0500

    added links to other files in directory

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [];
