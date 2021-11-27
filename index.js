bf539f61f1a8c780d1b5af2b8810f71f80eafab3
trm621 <t.maher621@gmail.com>
2021-11-27 01:42:24 -0500

    added questions

--- index.js
+++ index.js
@@ -3,7 +3,60 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the title of your project. (Required)',
+        validate: readMeTitle => {
+            if (readMeTitle) {
+                return true;
+            } else {
+                console.log('Please enter a title for your project!');
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What languages did you use to build this project? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for you project. (Required)',
+        validate: readMeDescription => {
+            if (readMeDescription) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Please provide a link to the deployed website.',
+    },
+    {
+        type: 'input',
+        name: 'preview-link',
+        message: 'Please provide a link to a preview screenshot of your project.',
+        validate: previewLink => {
+            if (previewLink) {
+                return true;
+            } else {
+                console.log('Please provide a link to a preview screenshot of the project!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'authors',
+        message: 'Please credit each person(s) that worked on this project.'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
